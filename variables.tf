eb967911ec899852ea5d2a1bd27fc7f3118ff028
ayyappavedavyasgudipati <ayyappavedavyas.gudipati@gmail.com>
2026-03-23 00:08:35 +0530

    first commit

---
+++ variables.tf
@@ -0,0 +1,36 @@
+variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "component" {
+    type = string
+}
+
+variable "app_version" {
+    type = string
+    default = "v3"
+}
+
+variable "rule_priority" {
+    
+}
+
+variable "domain_name" {
+    default = "opswithvyas.online"
+}
+
+variable "ec2" {
+    type = object({
+        ami = string
+        instance_type = string
+    })
+
+    default = {
+        ami = "ami-0220d79f3f480ecf5"
+        instance_type = "t3.micro"
+    }
+}

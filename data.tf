6a4c39ef6d66b52680f97a201e5f5f034f9b98e6
ayyappavedavyasgudipati <ayyappavedavyas.gudipati@gmail.com>
2026-04-05 12:41:04 +0530

    Update data.tf

--- data.tf
+++ data.tf
@@ -7,11 +7,11 @@ data "aws_ssm_parameter" "sg_id" {
 }
 
 data "aws_ssm_parameter" "backend_alb_listener_arn" {
-    name = "/${var.project}/${var.environment}/backend_alb_listener_arn"
+    name = "/${var.project}/${var.environment}/backend_alb_listner_arn"
 }
 
 data "aws_ssm_parameter" "frontend_alb_listener_arn" {
-    name = "/${var.project}/${var.environment}/frontend_alb_listener_arn"
+    name = "/${var.project}/${var.environment}/frontend_alb_listner_arn"
 }
 
 data "aws_ssm_parameter" "vpc_id" {

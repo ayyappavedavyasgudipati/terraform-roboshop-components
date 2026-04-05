70075821a777578745be1b9d28f8ff50ad84093c
ayyappavedavyasgudipati <ayyappavedavyas.gudipati@gmail.com>
2026-04-05 12:45:06 +0530

    Update data.tf

--- data.tf
+++ data.tf
@@ -6,11 +6,11 @@ data "aws_ssm_parameter" "sg_id" {
     name = "/${var.project}/${var.environment}/${var.component}_sg_id"
 }
 
-data "aws_ssm_parameter" "backend_alb_listener_arn" {
+data "aws_ssm_parameter" "backend_alb_listner_arn" {
     name = "/${var.project}/${var.environment}/backend_alb_listner_arn"
 }
 
-data "aws_ssm_parameter" "frontend_alb_listener_arn" {
+data "aws_ssm_parameter" "frontend_alb_listner_arn" {
     name = "/${var.project}/${var.environment}/frontend_alb_listner_arn"
 }
 
